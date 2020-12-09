81c48c4e7c19aa6380d78d33108aa12d93bc8072
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2020-12-09 17:10:42 -0300

    vou começar a testar alguns scripts

---
+++ java script/exercicios/ex001/test1.js
@@ -0,0 +1,12 @@
+var dia = new Date()
+var hora = dia.getHours()
+var hora = 9
+var mudar = document.querySelector(div.hm)
+if (hora<12) {
+    Fundo.background = '#1a7aa0'
+    
+} else if (hora<18) {
+    Fundo.background = '#8f1b38'
+} else {
+    Fundo.background = '#341786'
+}
