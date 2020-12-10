366718a3914e3ed228f869802f49ce1b66508748
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2020-12-10 13:35:26 -0300

    arrumando js denovo

--- java script/exercicios/ex001/test1.js
+++ java script/exercicios/ex001/test1.js
@@ -1,6 +1,7 @@
 function carregar() {
-    var msg = window.document.getElementById(i)
-    var imagem = window.document.getElementById(foto)
+    var msg = window.document.querySelector(div.msg)
+    var imagem = window.document.querySelector(div.i)
+    var msg = String(msg)
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
