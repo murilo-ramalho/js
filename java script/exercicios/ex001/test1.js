8e7e4e8dd0f898edf17bd25aabeb481df5f096eb
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2020-12-10 14:09:49 -0300

    terminei de arrumar e entendi algumas coisas

--- java script/exercicios/ex001/test1.js
+++ java script/exercicios/ex001/test1.js
@@ -1,7 +1,6 @@
 function carregar() {
-    var msg = window.document.querySelector(div.msg)
-    var imagem = window.document.querySelector(div.i)
-    var msg = String(msg)
+    var msg = window.document.getElementById('msg')
+    var imagem = window.document.getElementById('i')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
---
+++ java script/exercicios/tests/1.js
@@ -0,0 +1,4 @@
+function c() {
+    var m = document.getElementsByClassName(div.f)
+    m.inneHTML = 'oi'
+}
