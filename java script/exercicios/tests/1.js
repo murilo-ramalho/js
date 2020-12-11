bddeec101109cb2dba21fdb81fb88f0c0cb19db1
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2020-12-11 14:42:35 -0300

    to testando o conhecimento que adquiri

--- java script/exercicios/tests/1.js
+++ java script/exercicios/tests/1.js
@@ -1,4 +1,44 @@
-function c() {
-    var m = document.getElementsByClassName(div.f)
-    m.inneHTML = 'oi'
+function verificar() {
+    var diaS = Number(document.querySelector('input#diaE'))
+    var vS = document.getElementById('vE')
+    var mS = document.getElementById('mE')
+    var data = new Date()
+    var diaDaS = data.getDay()
+    switch(diaS) {
+        case 0:
+            window.alert('preencha o campo corretamente')
+            break
+        case 1:
+            mS.innerHTML = 'o dia digitado é domingo '
+            mS.innerHTML += `mais hoje é ${diaDaS}`
+            break
+        case 2:
+            mS.innerHTML = 'o dia digitado é segunda-feira'
+            mS.innerHTML += `mais hoje é ${diaDaS}`
+            break
+        case 3:
+            mS.innerHTML = 'o dia digitado é terça-feira '
+            mS.innerHTML += `mais hoje é ${diaDaS}`
+            break
+        case 4: 
+            mS.innerHTML = 'o dia digitado é quarta-feira '
+            mS.innerHTML += `mais hoje é ${diaDaS}`
+            break
+        case 5:
+            mS.innerHTML = 'o dia digitado é quinta-feira '
+            mS.innerHTML += `mais hoje é ${diaDaS}`
+            break
+        case 6:
+            mS.innerHTML = 'o dia digitado é sexta-feira '
+            mS.innerHTML += `mais hoje é ${diaDaS}`
+            break
+        case 7:
+            mS.innerHTML = 'o dia digitado é sábado'
+            mS.innerHTML += `mais hoje é ${diaDaS}`
+            break
+        default:
+            window.alert('preencha o campo correntamente')
+            break
+
+    }
 }
