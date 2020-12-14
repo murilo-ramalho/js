c4833bd889aae812261b9f9c01325c3a49b632c4
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2020-12-14 16:41:20 -0300

    update

--- java script/exercicios/tests/2.js
+++ java script/exercicios/tests/2.js
@@ -1,15 +1,7 @@
 function magica() {
     var anoS = document.getElementById('anoE')
     var mS = document.getElementById('mE')
-    var anoV1 = (anoS.value.length)%4
-    var anoV2 = (anoS.value.length)%100
-    var anoV3 = (anoS.value.length)%400
-    var anot = anoV1+anoV2+anoV3
-    switch (anot) {
-        case 0:
-            mS.innerHTML = `o ano digitado é um ano bissexto`
-            break
-        default:
-            mS.innerHTML = `o ano digitado não é um ano bissexto`
+    if ((anoS.Value)%4==0 && (anoS.Value)%100==0) {
+        mS.innerHTML = `o ano ${anoS.Value} é bissexto`
     }
 }
