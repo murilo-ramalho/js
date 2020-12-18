bd9a24a2fc7f12ea4bf4e29a26bf0f65a4ab9af8
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2020-12-18 18:30:45 -0300

    update

--- java script/exercicios/tests/1.js
+++ java script/exercicios/tests/1.js
@@ -1,29 +1,29 @@
 function verificar() {
     var dS = document.getElementById('dE')
     var mS = document.getElementById('mE')
-    switch(dS.value.length) {
-        case 0:
+    switch (Number(dS.value)) {
+        case 1:
             mS.innerHTML = `dia digitado é domingo `
             break
-        case 1:
+        case 2:
             mS.innerHTML = `o dia digitado é segunda-feira `
             break
-        case 2:
+        case 3:
             mS.innerHTML = `o dia digitado é terça-feira `
             break
-        case 3:
+        case 4:
             mS.innerHTML = `o dia digitado é quarta-feira `
             break
-        case 4: 
+        case 5: 
             mS.innerHTML = `o dia digitado é quinta-feira `
             break
-        case 5:
+        case 6:
             mS.innerHTML = `o dia digitado é sexta-feira `
             break
-        case 6:
+        case 7:
             mS.innerHTML = `o dia digitado é sábado `
             break
         default:
-            window.alert('preencha o campo correntamente')
+            window.alert('preencha corretamente')
     } 
 }
