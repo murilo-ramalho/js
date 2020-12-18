4a3a4fa38bf1e2438f1f6c7595e2b96f52b183e9
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2020-12-18 18:33:58 -0300

    Update 5.js

--- java script/exercicios/tests/5.js
+++ java script/exercicios/tests/5.js
@@ -1,6 +1,6 @@
 function magica() {
-    var n1S = document.getElementById('n1E').value
-    var n2S = document.getElementById('n2E').value
+    var n1S = Number(document.getElementById('n1E').value)
+    var n2S = Number(document.getElementById('n2E').value)
     var S = document.getElementById('E')
     var m = (n1S+n2S)/2
     S.innerHTML = `a media de ${n1S} e de ${n2S} é ${m}`
