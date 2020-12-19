e7b1d4e751623998d34192e49a4a82c62bf38673
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2020-12-19 17:30:09 -0300

    update

---
+++ java script/exercicios/tests/11.js
@@ -0,0 +1,8 @@
+function magica1() {
+    var fg = document.getElementsByClassName('fg')
+    var conta = document.getElementById('conta')
+    var S = document.getElementById('E')
+    if (rsex[6].checked) {
+        
+    }
+}
