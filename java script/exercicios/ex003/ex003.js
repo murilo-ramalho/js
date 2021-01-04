9e32342e59139a6a2c16267acc13221f6a676914
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2021-01-04 16:08:30 -0300

    update

--- java script/exercicios/ex003/ex003.js
+++ java script/exercicios/ex003/ex003.js
@@ -1,18 +1,20 @@
 function magica() {
-    var n1 = Number(document.getElementById('n1E').value)
-    var n2 = Number(document.getElementById('n2E').value)
-    var S = document.getElementById('E')
-    var ax = 0
-    if (n1<n2) {
-        ax = n2
-        n2 = n1
-        n1 = ax
-    }
-    for (var c = n1 ; c<n2 ; c++) {
-        if (c=n1) {
-            S.innerHTML = `${c}`
+    let n1 = Number(document.getElementById('n1E').value)
+    let n2 = Number(document.getElementById('n2E').value)
+    let p = Number(document.getElementById('pE').value)
+    let S = document.getElementById('E')
+    if (n1.lenght == 0 || n2.lenght == 0 || p.lenght == 0) {
+        window.alert('preencha corretamente')
+    } else {
+        S.innerHTML = ''
+        if (n1<n2) {
+            for (let c = n1; c<=n2; c += p) {
+                S.innerHTML += `▷ ${c}`
+            }
         } else {
-            S.innerHTML += `${c}`
+            for (let c = n1; c>=n2; c -= p) {
+                S.innerHTML += `▷ ${c}`
+            }
         }
     }
 }
