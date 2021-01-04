4326ff8a99e5011f225aa20560d56e5b00c9d1dc
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2021-01-04 19:02:15 -0300

    update

--- java script/exercicios/tests/15.js
+++ java script/exercicios/tests/15.js
@@ -1,12 +1,16 @@
 function magica() {
     let n = Number(document.getElementById('nE').value)
     let S = document.getElementById('E')
-    if (n.lenght == 0) {
+    if (n.lenght == 0 || n<0) {
         window.alert('prencha corretamente')
     } else {
-        S.innerHTML = `${n}. `
-        for (let c = n;c<=1 ;c--) {
+        S.innerHTML = `${n}! `
+        let ax = n
+        n--
+        for (let c = n;c>=1 ; c--) {
             S.innerHTML += `${c} `
+            ax *= c
         }
+        S.innerHTML += `! = ${ax}`
     }
 }
