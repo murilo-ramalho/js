c42d3fb259a13fa6dc1a02e08adcfd1b24647c04
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2021-01-06 13:49:25 -0300

    update

---
+++ java script/exercicios/ex005/ex005.js
@@ -0,0 +1,16 @@
+var s = document.getElementById('E')
+var nuns = document.getElementById('amosN')
+function magica1() {
+    let n = Number(document.getElementById('nsE').value)
+    if (n<0 || n>100 || n.length == 0) {
+        window.alert('preencha corretamente') 
+    } else {
+        a
+    }    
+}
+
+
+var ax = function auxiliar(x) {
+    nuns.createElement('p')
+    
+}
