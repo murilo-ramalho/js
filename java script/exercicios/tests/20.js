a3044f5899f7604bdb99f272abc6e4c79894edcf
murilo-ramalho <74578694+murilo-ramalho@users.noreply.github.com>
2021-01-28 00:48:08 -0300

    Update 20.js

--- java script/exercicios/tests/20.js
+++ java script/exercicios/tests/20.js
@@ -22,5 +22,38 @@ function animate() {
     cube.rotation.y += 0.05;
     renderer.render(scene,camera);
 }
+document.addEventListener("keydown", mover);
+
+function mover(event) {
+        switch (event.keyCode) {
+            case 37: //left 
+                camera.rotation.y -= 0.1;
+                break;
+            case 38: //up
+                camera.rotation.x += 0.1;
+                break;
+            case 39: //right 
+                camera.rotation.y += 0.1;
+                break;
+            case 40: //dowm
+                camera.rotation.x -= 0.1;
+                break;
+            case 87: // andar pra frente
+                camera.position.z -= 0.1
+                break; 
+            case 83: // andar pra traz
+                camera.position.z += 0.1
+                break; 
+            case 68: // andar pro lado direito
+                camera.position.x += 0.1
+                break; 
+            case 65: // andar pro lado esquerdo
+                camera.position.x -= 0.1
+                break;
+            default:
+                break;
+        }
+};
+
 //inicio do movimento
 animate();
